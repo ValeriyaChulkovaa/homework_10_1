07e6c6091c625def59104dbdcd816e16ca7200b1
Valeriya <chulkovav66@mail.ru>
2024-07-06 14:11:32 +0300

    finished homework 11.1

--- src/masks.py
+++ src/masks.py
@@ -8,11 +8,14 @@ def mask_account_card(number_card: str) -> Any:
     elif 'MasterCard' in str(number_card):
         return f"{number_card[:10]} {number_card[11:15]} {number_card[15:17]}** **** {number_card[23:]}"
     elif 'Visa Classic' in str(number_card):
-        return f"{number_card[:4]} {number_card[5:12]} {number_card[13:17]} {number_card[17:19]}** **** {number_card[25:]}"
+        return (f"{number_card[:4]} {number_card[5:12]} "
+                f"{number_card[13:17]} {number_card[17:19]}** **** {number_card[25:]}")
     elif 'Visa Platinum' in str(number_card):
-        return f"{number_card[:4]} {number_card[5:13]} {number_card[14:18]} {number_card[18:20]}** **** {number_card[26:]}"
+        return (f"{number_card[:4]} {number_card[5:13]}"
+                f" {number_card[14:18]} {number_card[18:20]}** **** {number_card[26:]}")
     elif 'Visa Gold' in str(number_card):
-        return f"{number_card[:4]} {number_card[5:9]} {number_card[10:14]} {number_card[14:16]}** **** {number_card[22:]}"
+        return (f"{number_card[:4]} {number_card[5:9]}"
+                f" {number_card[10:14]} {number_card[14:16]}** **** {number_card[22:]}")
     elif 'Счет' in str(number_card) or 'Счёт' in str(number_card):
         return f"{number_card[:4]} **{number_card[21:]}"
     else:
@@ -26,4 +29,3 @@ print(mask_account_card('Visa Platinum 8990922113665229'))
 print(mask_account_card('Visa Gold 5999414228426353'))
 print(mask_account_card('Счет 64686473678894779589'))
 print(mask_account_card('Счёт 64686473678894779589'))
-
---
+++ tests/test_processing.py
@@ -0,0 +1,98 @@
+import pytest
+
+from src.processing import filter_by_state, sort_by_date
+
+
+def test_filter_of_state_executed(
+    list_of_dist_filter_of_state: list[dict], list_of_dist_filter_of_state_result_state_executed: list[dict]
+) -> None:
+    assert filter_by_state(list_of_dist_filter_of_state) == list_of_dist_filter_of_state_result_state_executed
+
+
+def test_filter_of_state_canceled(
+    list_of_dist_filter_of_state: list[dict], list_of_dist_filter_of_state_result_state_canceled: list[dict]
+) -> None:
+    assert (
+        filter_by_state(list_of_dist_filter_of_state, "CANCELED") == list_of_dist_filter_of_state_result_state_canceled
+    )
+
+
+@pytest.mark.parametrize(
+    "list_of_dist, filter_state",
+    [
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        )
+    ],
+)
+def test_filter_by_state(list_of_dist: list[dict], filter_state: list[dict]) -> None:
+    assert filter_by_state(list_of_dist) == filter_state
+
+
+@pytest.mark.parametrize(
+    "list_of_dist, filter_state",
+    [
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            [
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+        )
+    ],
+)
+def test_filter_by_state_canceled(list_of_dist: list[dict], filter_state: list[dict]) -> None:
+    assert filter_by_state(list_of_dist, "CANCELED") == filter_state
+
+
+@pytest.mark.parametrize(
+    "list_of_dist, sorted_date, is_reverse",
+    [
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+            True,
+        ),
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            [
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            ],
+            False,
+        ),
+    ],
+)
+def test_sort_by_date(list_of_dist: list[dict], sorted_date: list[dict], is_reverse: bool) -> None:
+    assert sort_by_date(list_of_dist, is_reverse) == sorted_date
