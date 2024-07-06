a8aa9df4352ef0f538ce679bd1bfa70027b7a312
Valeriya <chulkovav66@mail.ru>
2024-07-06 10:00:43 +0300

    finished homework

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -4,10 +4,20 @@ import pytest
 @pytest.mark.parametrize("x", [7000792289606361, 8000522289606361, 700792289606361, ()])
 def test_get_mask_card_number(x):
     card_number_str = str(x)
-    assert f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
+    return f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
+
+
+assert test_get_mask_card_number(7000792289606361) == "7000 79** **** 6361"
+assert test_get_mask_card_number(8000522289606361) == "8000 79** **** 6361"
+assert test_get_mask_card_number(700792289606361) == "7007 79** **** 6361"
 
 
 @pytest.mark.parametrize("x", [73654108430135874305, 773654108430135874305, 3373654108430135874305, ()])
 def test_get_mask_account(x):
     account_number_str = str(x)
-    assert f"**{account_number_str[-4:]}"
+    return f"**{account_number_str[-4:]}"
+
+
+assert test_get_mask_account(73654108430135874305) == "**4305"
+assert test_get_mask_account(773654108430135874305) == "**4305"
+assert test_get_mask_account(3373654108430135874305) == "**4305"
