81c3b5391f55e86e8808cf1146f2ba0de846945b
Valeriya <chulkovav66@mail.ru>
2024-07-06 10:00:43 +0300

    finished homework

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -25,43 +25,21 @@ def state():
 
 
 @pytest.fixture
-def date():
-    return [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-        },
-        {
-            "id": 615064591,
-            "state": "CANCELED",
-            "date": "2018-10-14T08:21:33.419441",
-        },
-    ]
-
-
-@pytest.fixture
 def input_date():
     return "2018-07-11T02:26:18.671407"
 
 
 def test_filter_by_state(state):
-    assert filter_by_state(state)
-
-
-def test_filter_by_state_empty():
-    assert filter_by_state([])
+    assert filter_by_state(state) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                                      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                                      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+                                      ]
 
 
 def test_sort_by_date(date):
-    assert sort_by_date(date)
-
-
-def test_sort_by_date_empty():
-    assert sort_by_date([])
+    assert sort_by_date(date) == [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                                  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                                  {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
+                                  ]
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,27 +1,26 @@
-from src.widget import get_data
-from src.masks import get_mask_account, get_mask_card_number
+from src.masks import mask_account_card
 import pytest
+
+
+def test_get_data(data_today):
+    assert test_get_data(data_today) == ['2018-07-11T02:26:18.671407']
 
 
 @pytest.mark.parametrize("x",
                          ['Visa Platinum 8990922113665229', 'Счет 73654108430135874305', 'Maestro 1596837868705199', '']
                          )
-def test_mask_account_card(x):
-    account_number = x[x.rfind(' ') + 1:]
-
-    if 'Счет' in x:
-        return (
-                x[: x.rfind(' ')]
-                + ' '
-                + get_mask_account(account_number)
-        )
-    else:
-        return (
-                x[: x.rfind(' ')]
-                + ' '
-                + get_mask_card_number(account_number)
-        )
+def test_get_mask_card(x):
+    number_card = str(x)
+    if 'Visa Platinum' in str(number_card):
+        return (f"{number_card[:4]} {number_card[5:13]} "
+                f"{number_card[14:18]} {number_card[18:20]}** **** {number_card[26:]}")
+    elif 'Счет' in str(number_card) or 'Счёт' in str(number_card):
+        return f"{number_card[:4]} **{number_card[21:]}"
+    elif 'Maestro' in str(number_card):
+        return (f"{number_card[:7]} {number_card[8:12]} "
+                f"{number_card[12:14]}** **** {number_card[20:]}")
 
 
-def test_get_data(input_date):
-    assert get_data(input_date)
+assert test_get_mask_card('Visa Platinum 8990922113665229') == "8990 92** **** 5229"
+assert test_get_mask_card('Счет 73654108430135874305') == "7365 41** **** 4305"
+assert test_get_mask_card('Maestro 1596837868705199') == "1596 83** **** 5199"
