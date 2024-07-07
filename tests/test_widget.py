2af688fb027463cb68ad2c93e73f99eb98868217
Valeriya <chulkovav66@mail.ru>
2024-07-07 08:58:51 +0300

    tests correct finish

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,26 +1,8 @@
-from src.masks import mask_account_card
 import pytest
 
-
-def test_get_data(data_today):
-    assert test_get_data(data_today) == ['2018-07-11T02:26:18.671407']
+from src.widget import get_data
 
 
-@pytest.mark.parametrize("x",
-                         ['Visa Platinum 8990922113665229', 'Счет 73654108430135874305', 'Maestro 1596837868705199', '']
-                         )
-def test_get_mask_card(x):
-    number_card = str(x)
-    if 'Visa Platinum' in str(number_card):
-        return (f"{number_card[:4]} {number_card[5:13]} "
-                f"{number_card[14:18]} {number_card[18:20]}** **** {number_card[26:]}")
-    elif 'Счет' in str(number_card) or 'Счёт' in str(number_card):
-        return f"{number_card[:4]} **{number_card[21:]}"
-    elif 'Maestro' in str(number_card):
-        return (f"{number_card[:7]} {number_card[8:12]} "
-                f"{number_card[12:14]}** **** {number_card[20:]}")
-
-
-assert test_get_mask_card('Visa Platinum 8990922113665229') == "8990 92** **** 5229"
-assert test_get_mask_card('Счет 73654108430135874305') == "7365 41** **** 4305"
-assert test_get_mask_card('Maestro 1596837868705199') == "1596 83** **** 5199"
+@pytest.mark.parametrize("coded_date, decoded_date", [("2018-07-11T02:26:18.671407", "11.07.2018")])
+def test_get_data(coded_date: str, decoded_date: str) -> None:
+    assert get_data(coded_date) == decoded_date
