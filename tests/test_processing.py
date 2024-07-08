ad9ebea59616d6b00e8a52a314acde494f17a06b
Valeriya <chulkovav66@mail.ru>
2024-07-08 10:41:14 +0300

    tests correct

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,5 +1,6 @@
-from src.processing import filter_by_state, sort_by_date
 import pytest
+
+from src.processing import filter_by_state
 
 
 @pytest.fixture
@@ -31,15 +32,4 @@ def input_date():
 
 def test_filter_by_state(state):
     assert filter_by_state(state) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                                      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                                      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                                      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-                                      ]
-
-
-def test_sort_by_date(date):
-    assert sort_by_date(date) == [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                                  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                                  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-                                  {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
-                                  ]
+                                      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
