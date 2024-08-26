b79b35298563ca76d97511ca4aaac0bff5edd7c7
Valeriya <chulkovav66@mail.ru>
2024-08-26 20:23:25 +0300

    Course work

---
+++ src/product.py
@@ -0,0 +1,40 @@
+from src.base_product import BaseProduct
+from src.print_mixin import PrintMixin
+
+
+class Product(BaseProduct, PrintMixin):
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.quantity = quantity
+        super().__init__()
+
+    def __str__(self):
+        return f'{self.name}, {self.price} руб. Остаток: {self.quantity} шт.'
+
+    def __add__(self, other):
+        if type(other) is Product:
+            return self.price * self.quantity + other.price * other.quantity
+        raise TypeError
+
+    @property
+    def price(self):
+        return self.__price
+
+    @classmethod
+    def new_product(cls, dict_product):
+        return cls(dict_product['name'], dict_product['description'], dict_product['price'], dict_product['quantity'])
+
+    @price.setter
+    def price(self, new_price: int):
+        if new_price <= 0:
+            print('Цена не должна быть нулевая или отрицательная')
+            return
+        self.__price = new_price
+        
