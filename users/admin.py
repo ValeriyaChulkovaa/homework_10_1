1cc11c16232496c026a8d9746cba524efa7f8039
Valeriya <chulkovav66@mail.ru>
2025-05-23 22:13:32 +0300

    Homework 31

---
+++ users/admin.py
@@ -0,0 +1,12 @@
+from django.contrib.admin import register
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+
+
+@register(User)
+class CustomUserAdmin(UserAdmin):
+    """
+    Класс для отображения модели User в интерфейсе админки
+    """
+    list_display = ("id", "email", "username", "phone_number")
