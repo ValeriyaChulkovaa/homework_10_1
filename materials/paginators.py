64a674cd9e3b19ec7db541d278e7433ec896ec12
Valeriya <chulkovav66@mail.ru>
2025-06-10 19:36:37 +0300

    homework 26.1 - final, add files

--- materials/paginators.py
+++ materials/paginators.py
@@ -1,6 +1,19 @@
-from django.apps import AppConfig
+from rest_framework.pagination import PageNumberPagination
 
 
-class MaterialsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'materials'
+class CoursePaginator(PageNumberPagination):
+    """
+    Пагинатор для списка объектов Course
+    """
+    page_size = 5
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+
+class LessonPaginator(PageNumberPagination):
+    """
+    Пагинатор для списка объектов Lesson
+    """
+    page_size = 20
+    page_size_query_param = "page_size"
+    max_page_size = 1000
--- users/tests.py
+++ users/tests.py
@@ -1,25 +1,3 @@
-from rest_framework import serializers
+from django.test import TestCase
 
-from materials.serializers import PaymentSerializer
-from .models import User
-
-
-class NewUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = "__all__"
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["email", "username", "first_name", "last_name", "phone_number", "country", "avatar"]
-
-
-class UserDetailSerializer(serializers.ModelSerializer):
-    payments_history = PaymentSerializer(source="payments", many=True)
-
-    class Meta:
-        model = User
-        fields = ["email", "username", "first_name", "last_name", "phone_number",
-                  "country", "avatar", "payments_history"]
+# Create your tests here.
